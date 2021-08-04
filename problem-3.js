81e2a2c7bb9f11e9b6ea2c0d7efff12a2b329f6b
Moeen Mahmud <write2fahim007@gmail.com>
2021-08-04 18:32:25 +0600

    Print the multiplication table with 7

---
+++ problem-3.js
@@ -0,0 +1,10 @@
+/**
+ * Print the multiplication table with 7
+ */
+
+let toMultiplied = 7;
+
+for (let i = 1; i <= 10; i++) {
+  let table = toMultiplied * i;
+  console.log(toMultiplied + " X " + i + " = " + table);
+}
