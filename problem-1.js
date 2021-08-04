0ee202290ba3101ff254487bdb110464eac130a7
Moeen Mahmud <write2fahim007@gmail.com>
2021-08-04 17:51:52 +0600

    Print numbers from 1 to 10

---
+++ problem-1.js
@@ -0,0 +1,11 @@
+/**
+ * Coding Challenge #1: Print numbers from 1 to 10
+ */
+
+function counterNum(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+console.log(counterNum(10));
