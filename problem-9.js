4e3aad5bfb0e58897a735a5fc75062855148040b
Moeen Mahmud <write2fahim007@gmail.com>
2021-08-05 17:19:09 +0600

    Create a function that will convert from Fahrenheit to Celsiu

---
+++ problem-9.js
@@ -0,0 +1,10 @@
+/**
+ * Create a function that will convert from Fahrenheit to Celsius
+ */
+
+function toCelsius(temp) {
+  const celsius = (temp - 32) * (5 / 9);
+  return celsius;
+}
+
+console.log(toCelsius(89.6));
