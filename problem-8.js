234e84244849bb4d57702ae89ef1007132bf11af
Moeen Mahmud <write2fahim007@gmail.com>
2021-08-05 17:14:02 +0600

    Create afunction that will convert from Celsius to Fahrenheit

---
+++ problem-8.js
@@ -0,0 +1,10 @@
+/**
+ * Create a function that will convert from Celsius to Fahrenheit
+ */
+
+function toFahrenheit(temp) {
+  let fahrenheit = (temp * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+console.log(toFahrenheit(32));
